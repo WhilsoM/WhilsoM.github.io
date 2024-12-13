0ed6d43bfd08f5c9d0dc1584b32433bd0475f0c2
Arthur Akhmetov <artur.akhmetov.2008@gmail.com>
2024-12-13 21:44:01 +0300

    feat: change page number pagination navigation on infinite scroll

--- assets/scripts/pagination.js
+++ assets/scripts/pagination.js
@@ -1,18 +1,32 @@
-const contentDiv = document.getElementById('content')
-const loadMoreButton = document.getElementById('loadMore')
+export const contentDiv = document.querySelector('#content')
 const loader = document.querySelector('.loader')
+const sortingSelect = document.querySelector('#sorting')
+
+export const API_URL = 'https://6729edd66d5fa4901b6f05f6.mockapi.io/items'
 
 let currentPage = 1
 const itemsPerPage = 10
 
-const fetchData = async (page) => {
+// запрос данных
+const fetchData = async (page, sortBy, order) => {
 	try {
+		// проверяет кол-во элементов в мокапи и на сайте, чтобы не запускать лишний раз лоадер
+		const resDataLength = await fetch(API_URL)
+		const dataCheck = await resDataLength.json()
+		const dataCheckLength = dataCheck.length
+
+		const cardsLength = document.querySelectorAll('.card').length
+
+		if (dataCheckLength === cardsLength) return
+
 		loader.style.display = 'block'
 
-		const response = await fetch(
-			`https://6729edd66d5fa4901b6f05f6.mockapi.io/items?page=${page}&limit=${itemsPerPage}`
-		)
+		const url = `${API_URL}?page=${page}&limit=${itemsPerPage}&sortBy=${sortBy}&order=${order}`
+
+		const response = await fetch(url)
+
 		const data = await response.json()
+
 		renderData(data)
 	} catch (error) {
 		console.error('Ошибка при загрузке данных:', error)
@@ -21,7 +35,9 @@ const fetchData = async (page) => {
 	}
 }
 
-function renderData(data) {
+// сделать проверку: длину кол-ва данных в мокапи и длину сколько находиться на сайте если же это равно то делать return и не будет происходить лоадинг и ошибки
+
+export const renderData = (data) => {
 	data.forEach((item) => {
 		const article = document.createElement('article')
 		const img = document.createElement('img')
@@ -52,9 +68,20 @@ function renderData(data) {
 	})
 }
 
-loadMoreButton.addEventListener('click', () => {
-	currentPage++
-	fetchData(currentPage)
+// infinite scroll
+window.addEventListener('scroll', () => {
+	if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100) {
+		currentPage++
+		const sort = sortingSelect.value
+		fetchData(currentPage, sort)
+	}
 })
 
-fetchData(currentPage)
+sortingSelect.addEventListener('change', () => {
+	currentPage = 1
+	contentDiv.innerHTML = ''
+	const sort = sortingSelect.value
+	fetchData(currentPage, sort)
+})
+
+fetchData(currentPage, sortingSelect.value, 'popularity', 'desc')
